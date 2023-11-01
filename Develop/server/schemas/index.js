e0dc47626e442b1b96b245a195b94fe4554c0516
Mark Anthony <touya1313@hotmail.com>
2023-11-01 15:51:09 -0600

    imported/exported and defined index.js file in schemas

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,16 @@
+const { gql } = require('apollo-server-express');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// Defining a base type for my schema
+const rootTypeDefs = gql`
+  type Query {
+    _empty: String
+  }
+
+  type Mutation {
+    _empty: String
+  }
+`;
+
+module.exports = [rootTypeDefs, typeDefs, resolvers];
