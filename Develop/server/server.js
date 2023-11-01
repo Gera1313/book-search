6ef139b86691191f42c185bdb0350ce53752adfa
Mark Anthony <touya1313@hotmail.com>
2023-11-01 15:42:20 -0600

    started server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express'); // ADDED: Import Apollo Server
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const typeDefs = require('./schemas'); // ADDED: Import GraphQL type definitions
+const resolvers = require('./resolvers'); // ADDED: Import GraphQL resolvers
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +17,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+// ADDED 22-30. Create an Apollo Server instance
+const server = new ApolloServer({
+  typeDefs, // Use imported type definitions
+  resolvers, // Use imported resolvers
+  context: ({ req }) => ({ req }), // Pass the req object to the context for authentication
+});
+
+// Apply Apollo Server as middleware
+server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+// Modified this file. 
