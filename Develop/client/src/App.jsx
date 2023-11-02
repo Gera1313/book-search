b42e2c450b5dad985a6066408cb1bbbe98e46b29
Mark Anthony <touya1313@hotmail.com>
2023-11-02 00:17:17 -0600

    I think I have finished app.jsx. Not working tho. figure out later

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,15 +1,28 @@
+// import React from 'react'; // Added this
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'; // Added this
 import Navbar from './components/Navbar';
 
 function App() {
+  // Added line 9-16
+  const httpLink = createHttpLink({
+    uri: '/graphql' // URI of appollo goes here
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+// Also added line 19 and 22
