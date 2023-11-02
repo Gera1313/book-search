f9858d58426bd7c306faeabd5ee5f7ec2b1d3b20
Mark Anthony <touya1313@hotmail.com>
2023-11-01 23:26:20 -0600

    added queries.js file

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query GetMe {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// Added this file/code
