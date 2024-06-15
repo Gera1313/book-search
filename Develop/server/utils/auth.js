4d883fca7b944b7277504b25ebfda1d973542fd0
Mark Anthony <touya1313@hotmail.com>
2024-06-15 02:19:25 -0500

    auth.js

---
+++ Develop/server/utils/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+// set token secret and expiration date
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+// Apollo server authentication
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    //allows token to be sent via req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization; 
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+  }
+}
