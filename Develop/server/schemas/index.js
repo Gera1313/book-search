2decd9841d488265cd0627adb67a2bc9babd212d
Mark Anthony <touya1313@hotmail.com>
2024-06-16 00:51:36 -0500

    starting over with index.js

--- Develop/server/schemas/index.js
+++ Develop/server/schemas/index.js
@@ -1,18 +1,22 @@
-const { gql } = require('apollo-server-express');
-const typeDefs = require('./typeDefs');
-const resolvers = require('../schemas/resolvers');
 
-// Defining a base type for my schema
-const rootTypeDefs = gql`
-  type Query {
-    _empty: String
-  }
 
-  type Mutation {
-    _empty: String
-  }
-`;
 
-module.exports = [rootTypeDefs, typeDefs, resolvers];
 
-// Created this file
+
+
+// const { gql } = require('apollo-server-express');
+// const typeDefs = require('./typeDefs');
+// const resolvers = require('../schemas/resolvers');
+
+// // Defining a base type for my schema
+// const rootTypeDefs = gql`
+//   type Query {
+//     _empty: String
+//   }
+
+//   type Mutation {
+//     _empty: String
+//   }
+// `;
+
+// module.exports = [rootTypeDefs, typeDefs, resolvers];
