ba8caabd353fac8e4768f9c061dd38f327b4e783
Mark Anthony <touya1313@hotmail.com>
2024-06-16 01:19:42 -0500

    add user mutation

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -18,6 +18,12 @@ const resolvers = {
     },
 
     Mutation: {
+      addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+
       login: async (_, { email, password }) => { // Maybe put something in blank spaces later on? 
         // Implement user authentication logic here
 
