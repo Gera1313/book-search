345b2e0f565a528b196098895255355f289920be
Mark Anthony <touya1313@hotmail.com>
2024-06-18 00:33:20 -0500

    typedefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -10,6 +10,14 @@ type User {
     bookCount: Int
     savedBooks: [Book]
   }
+    type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
 
 `
 
