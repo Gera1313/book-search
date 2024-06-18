336da39f73386cc2e845a2e7067b5d9f93678ef9
Mark Anthony <touya1313@hotmail.com>
2024-06-18 00:31:13 -0500

    typedefs.js

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,6 +3,13 @@ const { gql } = require('apollo-server-express');
 // I define my GraphQL schema. Define the necessary Query and Mutation types.
 
 const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
 `
 
