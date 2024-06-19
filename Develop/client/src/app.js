afcb49cd7d8e472552b7e8e7f3f9bf2841ce15fc
Mark Anthony <touya1313@hotmail.com>
2024-06-19 00:46:14 -0500

    app.js

--- Develop/client/src/app.js
+++ Develop/client/src/app.js
@@ -7,3 +7,15 @@ import Navbar from "./components/Navbar";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
+const client = new ApolloClient({
+    request: (operation) => {
+      const token = localStorage.getItem("id_token");
+  
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : "",
+        },
+      });
+    },
+    uri: "/graphql",
+  });
