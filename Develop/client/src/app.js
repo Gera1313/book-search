0757f58d307873581ee9ec159dd6c0768911a7ad
Mark Anthony <touya1313@hotmail.com>
2024-06-19 23:46:06 -0500

    forgot brackets

--- Develop/client/src/app.js
+++ Develop/client/src/app.js
@@ -8,32 +8,33 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    request: (operation) => {
-      const token = localStorage.getItem("id_token");
-  
-      operation.setContext({
-        headers: {
-          authorization: token ? `Bearer ${token}` : "",
-        },
-      });
-    },
-    uri: "/graphql",
-  });
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
 
-  function App() {
-    return (
-      <ApolloProvider client={client}>
-        <Router>
-          <>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={SearchBooks} />
-              <Route exact path="/saved" component={SavedBooks} />
-              <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-            </Switch>
-          </>
-        </Router>
-      </ApolloProvider>
-    );
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
-    export default App;
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
