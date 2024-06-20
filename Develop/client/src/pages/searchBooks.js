7794ce336e073ec6416857ba172b99e54e20e29c
Mark Anthony <touya1313@hotmail.com>
2024-06-19 23:52:44 -0500

    searchbooks.js

--- Develop/client/src/pages/searchBooks.js
+++ Develop/client/src/pages/searchBooks.js
@@ -15,3 +15,14 @@ import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 import { useMutation } from "@apollo/react-hooks";
 import { SAVE_BOOK } from "../utils/mutations";
+
+const SearchBooks = () => {
+    // create state for holding returned google api data
+    const [searchedBooks, setSearchedBooks] = useState([]);
+    // create state for holding our search field data
+    const [searchInput, setSearchInput] = useState("");
+  
+    // create state to hold saved bookId values
+    const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  
+    const [saveBook] = useMutation(SAVE_BOOK);
