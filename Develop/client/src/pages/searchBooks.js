744e147b56b7cbc834c438bd4ae52fbe2c1388cb
Mark Anthony <touya1313@hotmail.com>
2024-06-20 18:45:57 -0500

    try

--- Develop/client/src/pages/searchBooks.js
+++ Develop/client/src/pages/searchBooks.js
@@ -40,4 +40,19 @@ const SearchBooks = () => {
       return false;
     }
 
-    
+    try {
+        const response = await searchGoogleBooks(searchInput);
+  
+        if (!response.ok) {
+          throw new Error("something went wrong!");
+        }
+  
+        const { items } = await response.json();
+  
+        const bookData = items.map((book) => ({
+          bookId: book.id,
+          authors: book.volumeInfo.authors || ["No author to display"],
+          title: book.volumeInfo.title,
+          description: book.volumeInfo.description,
+          image: book.volumeInfo.imageLinks?.thumbnail || "",
+        }));
