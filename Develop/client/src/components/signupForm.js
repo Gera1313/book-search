b0e5ba37e4a68cb8fb888746dbf580c34ab088ff
Mark Anthony <touya1313@hotmail.com>
2024-07-02 16:51:37 -0500

    file

--- Develop/client/src/components/signupForm.js
+++ Develop/client/src/components/signupForm.js
@@ -4,3 +4,24 @@ import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/react-hooks";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
+
+const SignupForm = () => {
+    // set initial form state
+    const [userFormData, setUserFormData] = useState({
+      username: "",
+      email: "",
+      password: "",
+    });
+    // set state for form validation
+    const [validated] = useState(false);
+    // set state for alert
+    const [showAlert, setShowAlert] = useState(false);
+  
+    const [createUser] = useMutation(ADD_USER);
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
+    };
+
+export default SignupForm;
