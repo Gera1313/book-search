5cb5273e064d250772ef372b7c1820ab4a839585
Mark Anthony <touya1313@hotmail.com>
2024-07-02 16:07:32 -0500

    savedbooks

--- Develop/client/src/pages/savedBooks.js
+++ Develop/client/src/pages/savedBooks.js
@@ -16,6 +16,9 @@ import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 
-
+const SavedBooks = () => {
+    const { loading, data } = useQuery(GET_ME);
+    const [removeBook] = useMutation(REMOVE_BOOK);
+    const userData = data?.me || [];
 
 export default SavedBooks;
