0b6e6a9f425c94577b900048000345f30ea4b58e
Mark Anthony <touya1313@hotmail.com>
2024-07-06 00:01:52 -0500

    notes

--- Develop/server/schemas/index.js
+++ Develop/server/schemas/index.js
@@ -4,24 +4,3 @@ const resolvers = require('./resolvers');
 
 // export the typeDefs and resolvers
 module.exports = { typeDefs, resolvers };
-
-
-
-
-
-// const { gql } = require('apollo-server-express');
-// const typeDefs = require('./typeDefs');
-// const resolvers = require('../schemas/resolvers');
-
-// // Defining a base type for my schema
-// const rootTypeDefs = gql`
-//   type Query {
-//     _empty: String
-//   }
-
-//   type Mutation {
-//     _empty: String
-//   }
-// `;
-
-// module.exports = [rootTypeDefs, typeDefs, resolvers];
