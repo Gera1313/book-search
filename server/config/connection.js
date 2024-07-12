96ed0110bebfc85016f6763f7d350fd1402c2746
Mark Anthony <touya1313@hotmail.com>
2024-07-11 23:08:35 -0500

    fixed some errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,13 +4,22 @@
 
 // module.exports = mongoose.connection;
 
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+// module.exports = mongoose.connection;
+
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
