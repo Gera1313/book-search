2ae10efd92f6ee42938b0205409469b055a5d9e9
Mark Anthony <touya1313@hotmail.com>
2024-07-20 13:07:19 -0600

    queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,3 @@
-// import { gql } from "@apollo/client";
-
-// export const GET_ME = gql`
-//   {
-//     me {
-//       _id
-//       username
-//       email
-//       savedBooks {
-//         bookId
-//         authors
-//         description
-//         title
-//         image
-//         link
-//       }
-//     }
-//   }
-// `;
-
-// Added this file/code
-
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
